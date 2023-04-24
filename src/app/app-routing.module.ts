2b972cda1865c734af187503b62039174e214740
victor-fernandezm <victor.fernandezm@softtek.com>
2023-04-24 11:21:25 +0200

    6. Routing
    Añadido HTML al componente del servicio del punto 5 para que aparezca algo en la pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InterpolationComponent } from './interpolation/interpolation.component';
+import { OneWayBindingComponent } from './one-way-binding/one-way-binding.component';
+import { EventsComponent } from './events/events.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import { HwServiceComponent } from './hw-service/hw-service.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: "interpolacion", component: InterpolationComponent},
+	{path: "owb", component: OneWayBindingComponent},
+	{path: "eventos", component: EventsComponent},
+	{path: "twb", component: TwoWayBindingComponent},
+	{path: "servicio", component: HwServiceComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
