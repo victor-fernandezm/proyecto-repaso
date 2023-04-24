60ba561363e1c0442f09e059ad25f6a4219a9fc5
victor-fernandezm <victor.fernandezm@softtek.com>
2023-04-24 10:48:44 +0200

    1. Interpolacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterpolationComponent
   ],
   imports: [
     BrowserModule,
